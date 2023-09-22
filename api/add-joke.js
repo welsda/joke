943bd650d3527927f57e81cd1fbb89a5deb231ce
Wellington Almeida <walmeida@mundolivre.digital>
2023-09-22 13:34:34 -0300

    test

--- api/add-joke.js
+++ api/add-joke.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
                 {
                   text: {
                     text: [
-                        `${req.body}`
+                        `${JSON.parse(req.body)}`
                     ]
                   }
                 }
