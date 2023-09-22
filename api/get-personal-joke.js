1ddf8c18019155ebd78e1a781c8457d8e2805dc6
Wellington Almeida <walmeida@mundolivre.digital>
2023-09-22 13:29:40 -0300

    test

--- api/get-personal-joke.js
+++ api/get-personal-joke.js
@@ -1,74 +1,9 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const Joke = mongoose.model('Joke', {
-//     list: String,
-//     text: String
-// });
-
-// exports.handler = async (context, event, callback) => { 
-//     const response = new Twilio.Response();
-//     response.appendHeader('Access-Control-Allow-Origin', '*');
-//     response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
-//     response.appendHeader('Content-Type', 'application/json');
-//     response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-//     try {
-//         await mongoose.connect(context.MONGO_DB_URI);
-                
-//         const jokes = await Joke.find({ list: 'pessoal' });
-
-//         if (jokes.length === 0) {
-//             response.setBody({
-//                 fulfillmentMessages: [
-//                     {
-//                       text: {
-//                         text: [
-//                             'Vc não tem piadas na sua lista'
-//                         ]
-//                       }
-//                     }
-//                 ]
-//             });
-//         } else {
-//             const randomJoke = jokes[Math.floor(Math.random() * jokes.length)].text;
-
-//             response.setBody({
-//                 fulfillmentMessages: [
-//                     {
-//                       text: {
-//                         text: [
-//                             randomJoke
-//                         ]
-//                       }
-//                     }
-//                 ]
-//             });
-//         }
-//     } catch (error) {
-//         response.setBody({
-//             fulfillmentMessages: [
-//                 {
-//                   text: {
-//                     text: [
-//                         'Não deu pra pegar uma piada da sua lista'
-//                     ]
-//                   }
-//                 }
-//             ]
-//         });
-//     } finally {
-//         await mongoose.disconnect();
-//     }
-
-//     return callback(null, response);
-// };
-
-// const mongoose = require('mongoose');
-
-// const Joke = mongoose.model('Joke', {
-//     list: String,
-//     text: String
-// });
+const Joke = mongoose.model('Joke', {
+    list: String,
+    text: String
+});
 
 module.exports = async (req, res) => { 
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -77,11 +12,11 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     try {
-        // await mongoose.connect(process.env.MONGO_DB_URI);
+        await mongoose.connect(process.env.MONGO_DB_URI);
                 
-        // const jokes = await Joke.find({ list: 'pessoal' });
+        const jokes = await Joke.find({ list: 'pessoal' });
 
-        // if (jokes.length === 0) {
+        if (jokes.length === 0) {
             return res.status(200).json({
                 fulfillmentMessages: [
                     {
@@ -93,21 +28,21 @@ module.exports = async (req, res) => {
                     }
                 ]
             });
-        // } else {
-        //     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)].text;
+        } else {
+            const randomJoke = jokes[Math.floor(Math.random() * jokes.length)].text;
 
-        //     return res.status(200).json({
-        //         fulfillmentMessages: [
-        //             {
-        //               text: {
-        //                 text: [
-        //                     randomJoke
-        //                 ]
-        //               }
-        //             }
-        //         ]
-        //     });
-        // }
+            return res.status(200).json({
+                fulfillmentMessages: [
+                    {
+                      text: {
+                        text: [
+                            randomJoke
+                        ]
+                      }
+                    }
+                ]
+            });
+        }
     } catch (error) {
         return res.status(500).json({
             fulfillmentMessages: [
@@ -120,8 +55,7 @@ module.exports = async (req, res) => {
                 }
             ]
         });
+    } finally {
+        await mongoose.disconnect();
     }
-    // } finally {
-    //     await mongoose.disconnect();
-    // }
 };
